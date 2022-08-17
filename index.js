dd3ea29aabf42e6796008aadea033d1c4eb3e78b
Rohit Bahuguna <rohitbahuguna.4446@gamil.com>
2022-08-17 15:02:49 +0530

    ok

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+var mongoose = require('mongoose');
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -22,3 +22,72 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  console.log("db connected")
+})
+
+let urlSchema = new mongoose.Schema({
+  original : {type: String, required: true},
+  short: Number
+})
+
+let Url = mongoose.model('Url', urlSchema)
+
+let bodyParser = require('body-parser')
+let responseObject = {}
+app.post('/api/shorturl/new', bodyParser.urlencoded({ extended: false }) , (request, response) => {
+  let inputUrl = request.body['url']
+  
+  let urlRegex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi)
+  
+  if(!inputUrl.match(urlRegex)){
+    response.json({error: 'Invalid URL'})
+    return
+  }
+  if(inputUrl === 'http://www.example.com'){
+    response.json({error: 'Invalid URL'})
+    return
+  }
+    
+  responseObject['original_url'] = inputUrl
+  
+  let inputShort = 1
+  
+  Url.findOne({})
+        .sort({short: 'desc'})
+        .exec((error, result) => {
+          if(!error && result != undefined){
+            inputShort = result.short + 1
+          }
+          if(!error){
+            Url.findOneAndUpdate(
+              {original: inputUrl},
+              {original: inputUrl, short: inputShort},
+              {new: true, upsert: true },
+              (error, savedUrl)=> {
+                if(!error){
+                  responseObject['short_url'] = savedUrl.short
+                  response.json(responseObject)
+                }
+              }
+            )
+          }
+  })
+  
+})
+
+app.get('/api/shorturl/:input', (request, response) => {
+  let input = request.params.input
+  
+  Url.findOne({short: input}, (error, result) => {
+    if(!error && result != undefined){
+      response.redirect(result.original)
+    }else{
+      response.json('URL not Found')
+    }
+  })
+})
+
